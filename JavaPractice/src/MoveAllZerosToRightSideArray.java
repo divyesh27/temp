545b800c3442c379a187e50e4c73a8ce51b289e4
Divyesh <divyesh.shah@salesforce.com>
2018-07-05 12:12:53 -0700

    Update the file with moving zero to front

--- JavaPractice/src/MoveAllZerosToRightSideArray.java
+++ JavaPractice/src/MoveAllZerosToRightSideArray.java
@@ -6,9 +6,10 @@ public class MoveAllZerosToRightSideArray {
 		int arr[] = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
         int n = arr.length;
         MoveZerosToEnd(arr, n);
-        System.out.println("Array after pushing zeros to the back: ");
-        for (int i=0; i<n; i++)
-            System.out.print(arr[i]+" ");
+        printArray(arr,n);
+        MoveZerosToStartAndOneToEnd(arr,n);
+        printArray(arr,n);
+        
 	}
 	public static void MoveZerosToEnd(int [] inputarray, int n)
 	{
@@ -20,6 +21,24 @@ public class MoveAllZerosToRightSideArray {
 		}
 		while(count < n)
 			inputarray[count++] = 0;
+	}
+	
+	public static void MoveZerosToStartAndOneToEnd(int[] inputarray, int n) {
+		int count = n-1;
+		for(int i=n-1; i>=0; i--) {
+			if(inputarray[i] != 0) {
+				inputarray[count--] = inputarray[i];
+			}
+		}
+		while(count >= 0)
+			inputarray[count--] = 0;
+	}
+	
+	public static void printArray(int[] inputarray, int n) {
+		System.out.println();
+		System.out.println("Array after pushing zeros to the back/front: ");
+        for (int i=0; i<n; i++)
+            System.out.print(inputarray[i]+" ");
 	}
 
 }
