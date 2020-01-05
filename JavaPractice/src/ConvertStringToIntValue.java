8dec2ba18b5c01dd34b36415af9755440e44ee2c
Divyesh <divyesh.shah@salesforce.com>
2020-01-05 10:25:06 -0800

    Add updated files convert string to int

--- JavaPractice/src/ConvertStringToIntValue.java
+++ JavaPractice/src/ConvertStringToIntValue.java
@@ -1,38 +1,58 @@
+
 public class ConvertStringToIntValue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(convertStringToIntegerValues("1242"));
-		//System.out.println(convertStringToIntegerValues("    acdd 234324234"));
-		//System.out.println(convertStringToIntegerValues(" acdd 234324234"));
-		//System.out.println(convertStringToIntegerValues("     21332 ad"));
+		System.out.println(convertStringToIntegerValues("    acdd 234324234"));
+		System.out.println(convertStringToIntegerValues(" acdd 234324234"));
+		System.out.println(convertStringToIntegerValues("     21332 ad"));
+		System.out.println(convertStringToIntegerValues("     21332"));
 		System.out.println(convertStringToIntegerValues("     -21332"));
+		System.out.println(convertStringToIntegerValues("-1242"));
+		System.out.println(convertStringToIntegerValues("-9343232242"));
+		System.out.println(convertStringToIntegerValues("9343232242"));
 	}
 
 	public static int convertStringToIntegerValues(String inputString) {
 
 		int negative = -1;
 		boolean flag = false;
-		StringBuilder sb = new StringBuilder(inputString.trim());
-		System.out.println(sb);
-		if(inputString == null || inputString.length() < 1 || Character.isLetter(sb.charAt(0))) {
+		int index=0;
+		if(inputString == null || inputString.length() < 1 || Character.isLetter(inputString.charAt(0))) {
 			throw new IllegalArgumentException("Pass Valid input");
-		
 		}
-		char [] inputCharArray = inputString.trim().toCharArray();
-		int result =0;
+		
+		inputString = inputString.trim();
+		char [] inputCharArray = inputString.toCharArray();
+		
+		double result = 0;
 		
 		if(inputCharArray[0] == '-') {
 			 flag = true;
+			 index = 1;
 		}
-		for(int i=0; i<inputCharArray.length; i++) {
+		if(inputCharArray[0] == '+') {
+			index = 1;
+		}
+		
+		for(int i=index ;i<inputCharArray.length; i++) {
 
-			if(Character.isAlphabetic(inputCharArray[i]) || Character.isLetter(inputCharArray[i]))
+			if(Character.isAlphabetic(inputCharArray[i]) || Character.isLetter(inputCharArray[i]) || Character.isSpaceChar(inputCharArray[i]))
 				break;
+			// if(inputCharArray[i] < '0' || inputCharArray[i] > '9') break;
 			int val = (inputCharArray[i]) - '0';
 			result = result*10 + val;
 		}
-		return result;
+		if(flag)
+			result = result*negative;
+		
+		if(result > Integer.MAX_VALUE)
+			return Integer.MAX_VALUE;
+		else if(result < Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+		else
+			return (int) result;
+		
 	}
-
 }
