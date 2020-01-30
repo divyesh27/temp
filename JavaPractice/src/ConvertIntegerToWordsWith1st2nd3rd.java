9c6614b28d706a272f5c037dfafba222a41a130c
Divyesh <divyesh.shah@salesforce.com>
2020-01-30 11:58:35 -0800

    Add new convert int to words

---
+++ JavaPractice/src/ConvertIntegerToWordsWith1st2nd3rd.java
@@ -0,0 +1,32 @@
+
+public class ConvertIntegerToWordsWith1st2nd3rd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		convertIntToWords(191);
+		convertIntToWords(1);
+		convertIntToWords(2);
+		convertIntToWords(3);
+		convertIntToWords(111);
+		convertIntToWords(112);
+		convertIntToWords(113);
+	}
+	
+	public static void convertIntToWords(int input) {
+		if(input > 200)
+			throw new IllegalArgumentException("Pass valid input");
+		
+		if(input % 10 == 1 && input/10 != 11)
+			System.out.print(input + "st\n");
+		
+		else if(input % 10 == 2 && input/10 != 11)
+			System.out.print(input + "nd\n");
+		
+		else if(input % 10 == 3 && input/10 != 11)
+			System.out.print(input + "rd\n");
+		
+		else
+			System.out.print(input + "th\n");
+	}
+
+}
