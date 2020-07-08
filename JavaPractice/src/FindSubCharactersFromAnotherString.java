a1ed459d586a65ee7848540e94fc3128e0a61f51
Divyesh <divyesh.shah@salesforce.com>
2020-07-07 21:28:19 -0700

    Add sub character from another string

---
+++ JavaPractice/src/FindSubCharactersFromAnotherString.java
@@ -0,0 +1,46 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindSubCharactersFromAnotherString {
+
+	//asbs%adsf^nsd(  --> asbsAdsfNsd
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(Character c : findSubChar("apple","pear"))
+			System.out.println(c);
+		
+		for(Character c : findSubChar("lock","door"))
+			System.out.println(c);
+		
+		for(Character c : findSubChar("foo","bar"))
+			System.out.println(c);
+		
+		for(Character c : findSubChar("foo",""))
+			System.out.println(c);
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Set<Character> findSubChar(String input1, String input2) {
+
+		if(input1 == null || input2 == null)
+			throw new IllegalArgumentException("Pass valid input");
+
+		if(input1.isEmpty() || input2.isEmpty())
+			return Collections.EMPTY_SET;
+
+		Set<Character> unique = new HashSet<Character>();
+		Set<Character> result = new HashSet<Character>();
+		for(Character c : input1.toCharArray()) 
+			unique.add(c);
+
+		for(Character c : input2.toCharArray()) {
+			if(unique.contains(c))
+				result.add(c);
+		}
+		
+		return result;
+	}
+
+}
