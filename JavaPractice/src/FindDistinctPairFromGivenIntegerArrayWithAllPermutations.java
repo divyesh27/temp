6732a58e2268ada9357624d3afedf5bf0bef6385
Divyesh <divyesh.shah@salesforce.com>
2020-07-07 21:27:48 -0700

    Add distinct pair of given integer with all permutations

---
+++ JavaPractice/src/FindDistinctPairFromGivenIntegerArrayWithAllPermutations.java
@@ -0,0 +1,36 @@
+
+import java.util.regex.*;
+
+/***
+ * 78. Subsets
+Medium
+216154FavoriteShare
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+Note: The solution set must not contain duplicate subsets.
+Example:
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+ * @author divyesh.shah
+ *
+ */
+public class FindDistinctPairFromGivenIntegerArrayWithAllPermutations {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
