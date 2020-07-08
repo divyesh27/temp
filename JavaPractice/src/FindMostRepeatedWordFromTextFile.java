2ce5d98958160a75e1f572350aee826ce2d049ad
Divyesh <divyesh.shah@salesforce.com>
2020-07-07 23:15:56 -0700

    Add comments

--- JavaPractice/src/FindMostRepeatedWordFromTextFile.java
+++ JavaPractice/src/FindMostRepeatedWordFromTextFile.java
@@ -7,6 +7,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * Given a a log file which consists of logging information like (ERROR, INFO, EXCEPTION, etc). Function
+ * needs to return list of words with the frequency of occurrence. 
+ * For e.g 
+ * "03/22 08:51:01 INFO   :...locate_configFile: Specified configuration file: /u/user10/rsvpd1.conf
+	03/22 08:51:01 INFO   :.main: Using log level 511
+	03/22 08:51:01 EXCEPTION   :..settcpimage: Get TCP images rc - EDC8112I Operation not supported on socket.
+	 03 
+	03/22 08:51:01 ERROR   :..settcpimage: Associate with TCP/IP image name = TCPCS
+	03/22 08:51:02 INFO   :..reg_process: registering process with the system
+	03/22 08:51:02 ERROR   :..reg_process: attempt OS/390 registration
+	03/22 08:51:02 INFO   :..reg_process: return from registration rc=0 
+ * @author divyesh.shah
+ *
+ */
 public class FindMostRepeatedWordFromTextFile {
 
 	public static void main(String[] args) {
