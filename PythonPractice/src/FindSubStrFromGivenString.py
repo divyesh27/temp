ed828b9dbef8e40c3702e854d1224be518e0c208
Divyesh <divyesh.shah@salesforce.com>
2024-09-09 11:05:24 -0700

    Make changes

--- PythonPractice/src/FindSubStrFromGivenString.py
+++ PythonPractice/src/FindSubStrFromGivenString.py
@@ -1,7 +1,7 @@
 #  Find the Index of the First Occurrence in a String
 
 class FindSubStrFromGivenString:
-    def strStr(haystack: str, needle: str) -> int:
+    def strStr(self, haystack: str, needle: str) -> int:
         if haystack is None or needle is None:
             raise ValueError ("Pass valid input")
 
@@ -9,6 +9,7 @@ class FindSubStrFromGivenString:
 
 
 if __name__ == "__main__":
-    print(FindSubStrFromGivenString.strStr("sadbutsad", "sad"))
-    print(FindSubStrFromGivenString.strStr("leetcode", "leeto"))
-    print(FindSubStrFromGivenString.strStr("sadbutsad", "but"))
+    fs = FindSubStrFromGivenString()
+    print(fs.strStr("sadbutsad", "sad"))
+    print(fs.strStr("leetcode", "leeto"))
+    print(fs.strStr("sadbutsad", "but"))
